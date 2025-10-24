d8b0da2f24f0f88dce52da5aefc83e0bde6b9cf2
Pedro Icaro <josepedroicaro@gmail.com>
2025-10-23 22:12:04 -0300

    Maior e Menor

--- Maior e Menor Valores.py
+++ Maior e Menor Valores.py
@@ -1,11 +1,11 @@
-from random import randint
+from random import randint 
 
-n = (randint(1, 10), randint(1,10), randint(1,10),randint(1,10),randint(1,10))
+n = (randint(1, 10), randint(1,10), randint(1,10),randint(1,10),randint(1,10)) #Randomiza vários valores dentro da tupla
 
 
-print (f"Os números sorteados foram: {n}")
-print (f"O maior número foi:{max(n)}")
-print (f"O menor número foi:{min(n)}")
+print (f"Os números sorteados foram: {n}") 
+print (f"O maior número foi:{max(n)}") #Puxa o maior valor randomizado
+print (f"O menor número foi:{min(n)}") #Puxa o menor valor randomizado
 
 
 
