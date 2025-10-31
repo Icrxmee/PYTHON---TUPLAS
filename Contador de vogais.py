261ba210d3a88bd02f538957e7c9073ce8d56496
Pedro Icaro <josepedroicaro@gmail.com>
2025-10-30 22:17:32 -0300

    Adjust: Documentação

--- Contador de vogais.py
+++ Contador de vogais.py
@@ -2,12 +2,12 @@ t = ("Jose","Pedro","Icaro","Mouta","Macedo")
 v = 'aeiou'
 
 
-for i in t:
-
+for i in t: #For para percorrer cada palavra.
+ 
     print (f"\nA palavra {i} tem as vogais: ", end="")
 
-    for n in i:
+    for n in i: #For para percorrer cada letra.
 
-        if n.lower() in 'aeiou':
+        if n.lower() in 'aeiou': #Joga tudo para minpusculo e analisa se tem vogal.
 
-            print (n, end="")
+            print (n, end="") #Diz as vogais que existem em cada palavra percorrida anteriormente.
